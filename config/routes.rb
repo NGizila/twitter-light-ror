8a91ee550807b6ff504303e2409a5f890ca7da7b
Jérémy Morel <jeremy.morel@etu.univ-grenoble-alpes.fr>
2021-03-18 09:48:20 +0100

    form doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "user#index"
+
+  get "/user", to: "user#index"
+  get "/user/new", to: "user#new"
+
+  resources :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
