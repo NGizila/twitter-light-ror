82d4559626d12b6c2e7fd87e5aa296ff5929cdcd
Jérémy Morel <jeremy.morel@etu.univ-grenoble-alpes.fr>
2021-04-07 09:48:04 +0200

    center horizontally edit user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   resources :tweets
   resources :users
+ 
   root "users#index"
-
-  get "/users", to: "user#index"
-  get "/users/new", to: "users#new"
-
-  resources :users do
-	resources :tweets
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
