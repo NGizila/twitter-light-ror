8612a46ef2d16eff92826bac701928e34dba44cb
Jérémy Morel <jeremy.morel@etu.univ-grenoble-alpes.fr>
2021-04-24 14:49:21 +0200

    add character limit + blank tweet not allowed anymore

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true
+  validates :content, length: { maximum: 240, too_long: "A tweet length can't superior than %{count} characters !" }
 end
