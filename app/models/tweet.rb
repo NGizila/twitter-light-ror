376ac980b6ffbd68bc08e10f59b518dd1e2704dc
Natalia Gizila <natalia.gizila@etu.univ-grenoble-alpes.fr>
2021-04-29 13:58:53 +0200

    error fix sur appel de js dans manifest

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,5 +4,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 240, too_long: "A tweet length can't superior than %{count} characters !" }
-  
 end
+
